218c1c63b1b661362bf3cf727127d6003df0fec4
Nikhil Kumar <nikhilshaw575@gmail.com>
2025-11-03 12:31:04 +0530

    basic setup done -> added security , cors,helmet, cookie-parser

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 import express from "express"
 import morgan from "morgan" 
+import rateLimit from "express-rate-limit"
+import helmet from "helmet"
+import mongoSanitize from 'express-mongo-sanitize'
+import hpp from "hpp"
+import cookieParser from "cookie-parser"
+import cors from 'cors'
 
 import dotenv from "dotenv"
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT
+
+
+// Global rate limiting 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per windowMs
+    message: "Too many requests from this IP, please try again later.",
+    standardHeaders: true, // Enable `RateLimit-*` headers
+    legacyHeaders: false, // Disable `X-RateLimit-*` headers
+})
+
+// security  middleware
+app.use(helmet())
+app.use(mongoSanitize())
+app.use(hpp())
+app.use("/api",limiter) ; // where ever the api is called in the search then enject the limiter
+
 
 
 // logging middleware
@@ -16,6 +39,7 @@ if(process.env.NODE_ENV==="development"){
 // Body Parser Middleware
 app.use(express.json({limit:'10kb'}))
 app.use(express.urlencoded({extended:true,limit:'10kb'}))
+app.use(cookieParser())
 
 // Global Error Handler
 app.use((err,req,res,next)=>{
@@ -27,6 +51,19 @@ app.use((err,req,res,next)=>{
     })
 })
 
+app.use(cors({
+    origin:process.env.CLIENT_URL || "https://localhost:5173",
+    credentials:true,
+    methods:["GET","POST","DELETE","PATCH","HEAD","OPTIONS"],
+    allowedHeaders:[
+        "Content-type",
+        "Authorization",
+        "X-Requested-With",
+        "device-remember-token",
+        "Orgin",
+        "Accept"
+    ]
+}))
 
 // API Routes
 
