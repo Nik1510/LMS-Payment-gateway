aab49ac1a67b0e4769888c46e91ca0966ef5f81e
Nikhil Kumar <nikhilshaw575@gmail.com>
2025-11-11 11:33:19 +0530

    refractor cloudinary.js

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,40 +1,40 @@
-import {v2 as cloudinary} from 'cloudinary'
-import dotenv from "dotenv"
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
 
-dotenv.config({})
+dotenv.config();
 
 cloudinary.config({
-    api_key : process.env.api_key,
-    api_secret: process.env.API_SECRET,
-    cloud_name : process.env.CLOUD_NAME
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+  cloud_name: process.env.CLOUD_NAME,
 });
 
-export const uploadMedia= async(file)=>{
-    try {
-        const uploadResponse = await cloudinary.uploader.upload(file,{
-            resource_type:'auto'
-        })
-        return uploadResponse;
-    } catch (error) {
-        console.log("Error while uploading media to cloudinary")
-        console.log(error)
-    }
-}
+export const uploadMedia = async (file) => {
+  try {
+    const uploadResponse = await cloudinary.uploader.upload(file, {
+      resource_type: "auto",
+    });
+    return uploadResponse;
+  } catch (error) {
+    console.log("Error while uploading media to Cloudinary");
+    console.error(error);
+  }
+};
 
-export const deleteMediaFromCloudinary = async(publicId)=>{
-    try {
-        await cloudinary.uploader.destroy(publicId)
-    } catch (error) {
-        console.log("Failed to delete MEDIA from Cloudinary")
-        console.log(error)
-    }
-}
+export const deleteMediaFromCloudinary = async (publicId) => {
+  try {
+    await cloudinary.uploader.destroy(publicId);
+  } catch (error) {
+    console.log("Failed to delete media from Cloudinary");
+    console.error(error);
+  }
+};
 
-export const deleteVideoFromCloudinary = async(publicId)=>{
-    try {
-        await cloudinary.uploader.destroy(publicId,{resource_type:"video"})
-    } catch (error) {
-        console.log("Failed to delete MEDIA from Cloudinary")
-        console.log(error)
-    }
-}
+export const deleteVideoFromCloudinary = async (publicId) => {
+  try {
+    await cloudinary.uploader.destroy(publicId, { resource_type: "video" });
+  } catch (error) {
+    console.log("Failed to delete video from Cloudinary");
+    console.error(error);
+  }
+};
